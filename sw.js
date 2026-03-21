b276c9effd37bb8d4e89f63680535086fce5ebb4
issacs83 <issacs.park@medit.com>
2026-03-21 23:20:47 +0900

    v3.8.1: 게임 멈춤 버그 수정 (try-catch + endingTriggered 리셋 + abilityState 방어코드 + 디버그 표시)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// 동그라미 런 Service Worker v3.8.0
-const CACHE_VERSION = 'v3.8.0';
+// 동그라미 런 Service Worker v3.8.1
+const CACHE_VERSION = 'v3.8.1';
 const CACHE_NAME = 'dongrami-run-' + CACHE_VERSION;
 const ASSETS = [
     '/dongrami-run/',
