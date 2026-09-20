6b041f43f52345c13d242b3aa19bdec3bb6db483
issacs83 <issacs.park@medit.com>
2026-09-20 02:00:37 -0700

    v3.19.0: 물음표미 캐릭터 추가

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// 동그라미 런 Service Worker v3.18.0
-const CACHE_VERSION = 'v3.18.0';
+// 동그라미 런 Service Worker v3.19.0
+const CACHE_VERSION = 'v3.19.0';
 const CACHE_NAME = 'dongrami-run-' + CACHE_VERSION;
 const ASSETS = [
     '/dongrami-run/',
